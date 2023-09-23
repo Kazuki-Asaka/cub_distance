6abefc5e1a0024dd0ec5987acc93ddfe364eb25b
akazuki <kazuki200317kazu@gmail.com>
2023-09-23 21:33:11 +0900

    re

--- distance/culc_distsnce.c
+++ distance/culc_distsnce.c
@@ -63,22 +63,56 @@ t_state_cor	set_init_cor_x_axis(t_cor *cor, double angle)
 	int			dx;
 	int			dy;
 	t_state_cor cor_init;
-	double	tmp;
+	double		tmp;
 
-	dx = cor -> x_now;
-	dy = cor -> y_now;
+	dx = (int)cor -> x_now;
+	dy = (int)cor -> y_now;
 	if (((double)0 < angle && angle < (double)90)
 	|| ((double)90 < angle && angle <(double)180))
 		dy++;
-	if (dy > tmp)
-		tmp = dy - cor -> y_now;
+	if (dy > cor -> y_now)
+		tmp = (double)dy - cor -> y_now;
 	else
-		tmp = cor -> y_now - dy;
-	if (((double)90 < angle && angle < (double)180)
+		tmp = cor -> y_now - (double)dy;
+	if (((double)180 < angle && angle < (double)270)
 	|| ((double)270 < angle && angle <(double)360))
 		tmp = -tmp;
 	cor_init.init_y = (double)dy;
 	cor_init.init_x = cor -> x_now + tmp / tan (angle * 3.14 / 180);
+	return (cor_init);
+}
+
+t_state_cor	set_init_cor_y_axis(t_cor *cor, double angle)
+{
+	int			dx;
+	int			dy;
+	t_state_cor cor_init;
+	double		tmp;
+
+	dx = (int)cor -> x_now;
+	dy = (int)cor -> y_now;
+	if (((double)0 < angle && angle < (double)90)
+	|| ((double)270 < angle && angle <(double)360))
+		dx++;
+	if (dx > cor -> x_now)
+		tmp =(double)dx - cor -> x_now;
+	else
+		tmp =  cor -> x_now - (double)dx;
+	if (((double)90 < angle && angle < (double)180)
+	|| ((double)180 < angle && angle <(double)270))
+		tmp = -tmp;
+	cor_init.init_x = (double)dx;
+	cor_init.init_y = cor -> y_now + tmp * tan(angle * 3.14 / 180);
+	return (cor_init);
+}
+
+int	init_check(t_state_cor cor_init)//マップによって異なる
+{
+	if (((double)0 <= cor_init.init_x && cor_init.init_x <= (double)5)
+	&& ((double)0 <= cor_init.init_y && cor_init.init_y <= (double)5))
+		return (0);
+	else
+		return (1);
 }
 
 t_state_cor	culc_intersection_x_axis(t_cor *cor, double angle)
@@ -88,7 +122,19 @@ t_state_cor	culc_intersection_x_axis(t_cor *cor, double angle)
 
 	delta_y = set_delta_y(angle);
 	cor_init = set_init_cor_x_axis(cor, angle);
-	
+	printf("angle %f x_axis_init (%f, %f)\n", angle, cor_init.init_x, cor_init.init_y);
+	if (init_check(cor_init) == 1)
+		return (cor_init);
+	while (check_map_x_axis(cor, cor_init.init_x, cor_init.init_y, angle) != '1')
+	{
+		cor_init.init_x = cor_init.init_x + delta_y;
+		if (((double)0 < angle && angle < (double)90)
+		|| ((double)90 < angle && angle < (double)180))
+			cor_init.init_y ++;
+		else
+			cor_init.init_y--;
+	}
+	return (cor_init);
 }
 
 t_state_cor	culc_intersection_y_axis(t_cor *cor, double angle)
@@ -97,6 +143,70 @@ t_state_cor	culc_intersection_y_axis(t_cor *cor, double angle)
 	double		delta_x;
 
 	delta_x = set_delta_x(angle);
+	cor_init = set_init_cor_y_axis(cor, angle);
+	printf("angle %f y_axis_init (%f, %f)\n", angle, cor_init.init_x, cor_init.init_y);
+	if (init_check(cor_init) == 1)
+		return (cor_init);
+	while (check_map_y_axis(cor, cor_init.init_x, cor_init.init_y, angle) != '1')
+	{
+		cor_init.init_y = cor_init.init_x + delta_x;
+		if (((double)0 < angle && angle < (double)90)
+		|| ((double)270 < angle && angle < (double)360))
+			cor_init.init_x ++;
+		else
+			cor_init.init_y--;
+	}
+	return (cor_init);
+}
+
+t_state_cor	multiple_parallel_intersection(t_cor *cor, double angle)
+{
+	t_state_cor	cor_init;
+	int			x_int;
+
+	x_int = cor -> x_now;
+	cor_init.init_y = cor -> y_now;
+	if (angle == (double) 0)
+		x_int ++;
+	cor_init.init_x = (double)x_int;
+	printf("angle %f x_axis_init (%f, %f)\n", angle, cor_init.init_x, cor_init.init_y);
+	while (check_map_y_axis(cor, cor_init.init_x, cor_init.init_y, angle) != '1')
+	{
+		if (angle == 0)
+			cor_init.init_x = cor_init.init_x + (double)1;
+		else
+			cor_init.init_x = cor_init.init_x - (double)1;
+	}
+	if (angle == (double)0)
+		cor_init.distance = cor_init.init_x - cor -> x_now;
+	else
+		cor_init.distance = cor_init.init_x;
+	return (cor_init);
+}
+
+t_state_cor	multiple_vertical_intersection(t_cor *cor, double angle)
+{
+	t_state_cor	cor_init;
+	int			y_int;
+
+	y_int = cor -> x_now;
+	cor_init.init_x = cor -> x_now;
+	if (angle == (double) 90)
+		y_int ++;
+	cor_init.init_y = (double)y_int;
+	printf("angle %f y_axis_init (%f, %f)\n", angle, cor_init.init_x, cor_init.init_y);
+	while (check_map_y_axis(cor, cor_init.init_x, cor_init.init_y, angle) != '1')
+	{
+		if (angle == 90)
+			cor_init.init_y = cor_init.init_y + (double)1;
+		else
+			cor_init.init_y = cor_init.init_y - (double)1;
+	}
+	if (angle == (double)270)
+		cor_init.distance = cor_init.init_y - cor -> y_now;
+	else
+		cor_init.distance = cor_init.init_y;
+	return (cor_init);
 }
 
 void	step_position(t_cor *cor, double angle)
@@ -104,12 +214,21 @@ void	step_position(t_cor *cor, double angle)
 	t_state_cor	intersection_x_axis;
 	t_state_cor	intersection_y_axis;
 
-	if (angle / (double)90 == 0)
-		multiple_ninty_inertaction(cor, angle);
+	if (angle == (double)0 || angle == (double)180)
+	{
+		intersection_y_axis = multiple_parallel_intersection(cor, angle);
+		printf("angle %f y_axis (%f, %f)\n", angle, intersection_y_axis.init_x, intersection_y_axis.init_y);
+	}
+	else if (angle == (double)90 || angle == (double)270)
+	{
+		intersection_x_axis = multiple_vertical_intersection(cor, angle);
+		printf("angle %f x_axis (%f, %f)\n", angle, intersection_x_axis.init_x, intersection_x_axis.init_y);
+	}
 	else
 	{
 		intersection_x_axis = culc_intersection_x_axis(cor, angle);
 		intersection_y_axis = culc_intersection_y_axis(cor, angle);
+		printf("angle %f x_axis (%f, %f) y_axis(%f, %f)\n", angle, intersection_x_axis.init_x, intersection_x_axis.init_y, intersection_y_axis.init_x, intersection_y_axis.init_y);
 	}
 }
 
